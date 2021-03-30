5d6c2fd1d88720f774d14d7e8a1828a1e22b8c70
soup423 <kent35103@gmail.com>
2021-03-30 22:09:47 +0300

    les

---
+++ main.js
@@ -0,0 +1,84 @@
+// console.log(2)
+// console.log(3)
+// var a=5;
+// console.log(a);
+// console.log(a);
+// var a=5
+// console.log(a)
+// a=2
+// console.log(a)
+// a="man" 
+// var a=5+2-4
+
+// console.log(a)
+// a=a+"mama"
+// console.log(a)
+// var a=(3+1)/2
+// console.log(a)
+// var b=5/3
+// console.log()
+// 7%3
+// var a=5
+// a=a+1 //это равно//
+// a++//этому//
+// var a=5;
+// a++;
+// console.log(a);
+// var b=6;
+// console.log(b++);
+// console.log(1)
+// if (3>1) {
+//     console.log(2)
+// }
+// if (3<1) {
+//     console.log(3)
+// }
+// console.log(4)
+// var a=17
+// if (a>=18) {
+//     console.log("Привет")
+// }
+// else {
+//     console.log("иди отсюда")
+// }
+// if (a>=18&&a<=21) {
+//     console.log("privet")
+// }
+// else if(a>21) {
+//     console.log("zdorou")
+// }
+// else {
+//     console.log("poka")
+// }
+// var a=3
+// var b=5
+// var c
+// if (a===b) {
+//     c=a+b
+    
+// }
+// else if (b>a) {
+//     c=b-a
+    
+// }
+// else {
+//     c=4
+  
+// }
+// console.log(c)
+var a=3
+var b=4
+if (a>b) {
+    console.log(a+b)
+}
+else {
+    a=a+b;
+    
+}
+if (a>b) {
+    console.log(a+b)
+}
+else {
+    a=a-b
+}
+console.log(a)
